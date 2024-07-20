885d70c77dda4dbce3e77c07dc22628231f1b5b3
peter <peterwangyixing@gmail.com>
2024-07-20 16:44:58 +0800

    Migrate to swift 5

--- Handwriting/MainView.swift
+++ Handwriting/MainView.swift
@@ -74,7 +74,7 @@ class MainView: UIView {
         outputTitleLabel.textAlignment = .center
         
         // Output
-        outputLabel.font = UIFont.systemFont(ofSize: 100, weight: UIFontWeightLight)
+        outputLabel.font = UIFont.systemFont(ofSize: 100, weight: UIFont.Weight.light)
         outputLabel.textAlignment = .center
         
         // Confidence
